f38b47926c30bbc04bbbd2b8c990e39fad3802ff
Kori Singularis <SkyShredder89@gmail.com>
2025-04-25 18:13:41 +0100

    Initial Commit
    20 new seals
    17 new spectrals
    8 new booster packs
    3 new tags

---
+++ localization/en-us.lua
@@ -0,0 +1,388 @@
+return {
+    descriptions = {
+        Back = {
+        },
+        Blind = {
+        },
+        Joker = {
+        },
+        Planet = {
+        },
+        Spectral = {
+            c_sillyseals_multivision = {
+                name = 'Multi-Vision',
+	        	text = {
+                    "Add a {C:red}Hot Pink Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.75{}", 
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : gudusername_53951{}'
+		        }
+            },
+            c_sillyseals_abyss = {
+                name = 'Abyss',
+	        	text = {
+                    "Add a {C:red}Deep Blue Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.75{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : gudusername_53951{}'
+		        }
+            },
+            c_sillyseals_resurrection = {
+                name = 'Resurrection',
+	        	text = {
+                    "Add a {C:red}Lilac Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.75{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : AlexZGreat{}'
+		        }
+            },
+            c_sillyseals_reinforce = {
+                name = 'Reinforce',
+	        	text = {
+                    "Add a {C:red}Magenta Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.75{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : LFMoth{}'
+		        }
+            },
+            c_sillyseals_empower = {
+                name = 'Empower',
+	        	text = {
+                    "Add a {C:red}Lime Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.75{}", 
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : gudusername_53951{}'
+		        }
+            },
+            c_sillyseals_assortment = {
+                name = 'Assortment',
+	        	text = {
+                    "Add a {C:red}Neapolitan Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.75{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : astralightsky{}'
+		        }
+            },
+            c_sillyseals_geode = {
+                name = 'Geode',
+	        	text = {
+                    "Add a {C:red}Gemstone Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.75{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : LFMoth{}'
+		        }
+            },
+            c_sillyseals_hellfire = {
+                name = 'Hellfire',
+	        	text = {
+                    "Add a {C:cry_epic}Infernal Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.4{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : astralightsky{}'
+		        }
+            },
+            c_sillyseals_sunrays = {
+                name = 'Sunrays',
+	        	text = {
+                    "Add a {C:cry_epic}Solar Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.4{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : astralightsky{}'
+		        }
+            },
+            c_sillyseals_dial = {
+                name = 'Dial',
+	        	text = {
+                    "Add a {C:cry_epic}Switch Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.4{}"
+		        }
+            },
+            c_sillyseals_palette = {
+                name = 'Palette',
+	        	text = {
+                    "Add a {C:cry_epic}Primary Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.4{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : astralightsky{}'
+		        }
+            },
+            c_sillyseals_deposit = {
+                name = 'Deposit',
+	        	text = {
+                    "Add a {C:cry_epic}Platinum Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0.4{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : LFMoth{}'
+		        }
+            },
+            c_sillyseals_royalty = {
+                name = 'Royalty',
+	        	text = {
+                    "Add a {C:legendary}Peach Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : astralightsky{}'
+		        }
+            },
+            c_sillyseals_spectrum = {
+                name = 'Spectrum',
+	        	text = {
+                    "Add a {C:legendary}Rainbow Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : AlexZGreat{}'
+		        }
+            },
+            c_sillyseals_quadrants = {
+                name = 'Quadrants',
+	        	text = {
+                    "Add a {C:legendary}Wild Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : astralightsky{}'
+		        }
+            },
+            c_sillyseals_sirius = {
+                name = 'Sirius',
+	        	text = {
+                    "Add a {C:legendary}Midnight Seal{} to {C:attention}#1#{}",
+                    "selected card(s) in your", 
+                    "hand, multiplies money",
+                    "by {X:money,C:white}X0{}",
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : astralightsky{}'
+		        }
+            },
+            c_sillyseals_ringularity = {
+                name = 'Ringularity',
+	        	text = {
+                    "Add a random {C:cry_exotic}Exotic Seal{}",
+                    "to {C:attention}#1#{} selected card",
+                    "in your hand", 
+                    '{C:dark_edition,s:0.7,E:2}Sprite by : gudusername_53951{}'
+		        }
+            },
+        },
+        Tag = {
+        },
+        Tarot = {
+        },
+        Voucher = {
+        },
+        Other = {
+            sillyseals_hotpinkseal_seal = {
+                name = 'Hot Pink Seal',
+	        	text = {
+	        		'Retrigger this card {C:attention}#1#{} time(s) for',
+	        		'{C:attention}every{} card in the played hand'
+	        	}
+            },
+            sillyseals_deepblueseal_seal = {
+                name = 'Deep Blue Seal',
+                text = {
+                    'Create {C:attention}#1#{} {C:edition}Negative{} {C:planet}Planet card(s)',
+                    'for final played {C:attention}poker hand{} of',
+                    'round if it is your {C:attention}most{} played',
+                    '{C:attention}poker hand{} and this card is {C:attention}held{}',
+                    'in hand'
+                }
+            },
+            sillyseals_lilacseal_seal = {
+                name = 'Lilac Seal',
+                text = {
+                    'Create {C:attention}#1#{} {C:edition}Negative{} {C:spectral}Spectral{}',
+                    'card(s) for {C:attention}each{} card with this',
+                    'seal if {C:attention}#2#{} or more cards with this',
+                    'seal are {C:attention}discarded{} at {C:attention}the{}',
+			        '{C:attention}same time{}'
+                }
+            },
+            sillyseals_magentaseal_seal = {
+                name = 'Magenta Seal',
+                text = {
+                    '{C:attention}Upgrade{} played {C:attention}poker hand{} by',
+                    '{C:attention}twice{} the number of {C:blue}hands{} left',
+                    'when {C:attention}scored{}'
+                }
+            },
+            sillyseals_limeseal_seal = {
+                name = 'Lime Seal',
+	        	text = {
+                    '{C:attention}Upgrade{} the {C:edition}edition{} on this',
+                    'card when {C:attention}played and scoring',
+                    '{C:inactive}(Uses order in the collection)'
+	        	}
+            },
+            sillyseals_neapolitanseal_seal = {
+                name = 'Neapolitan Seal',
+	        	text = {
+	        		'{C:attention}Create #1# {C:colourcard}Colour{} card(s) if',
+	        		'used in a {C:attention}secret poker hand'
+	        	}
+            },
+            sillyseals_gemstoneseal_seal = {
+                name = 'Gemstone Seal',
+	        	text = {
+                    'Earn {C:attention}#1#x{} the amount of times',
+                    'the {C:attention}played poker hand{} has been played',
+                    'as {X:gold,C:white}${} when scored',
+                    '{C:dark_edition,s:0.7,E:2}Seal art by : gudusername_53951{}'
+	        	}
+            },
+            sillyseals_infernalseal_seal = {
+                name = 'Infernal Seal',
+                text = {
+                    'Gives {C:attention}#1# Quintuple Tag(s){} if',
+                    'held in hand and your score',
+                    '{C:attention}catches on fire'
+                }
+            },
+            sillyseals_solarseal_seal = {
+                name = 'Solar Seal',
+                text = {
+	            	'Contributes {X:gold,C:white}#1#{} power',
+	            	'to {C:attention}Ascended{} hands'
+                }
+            },
+            sillyseals_switchseal_seal = {
+                name = 'Switch Seal',
+                text = {
+                    '{C:attention}Applies{} the {C:edition}edition{} on this card',
+                    'to {C:attention}all other scored cards{} and {C:attention}#1# random',
+                    '{C:attention}joker(s){} in your possession when scored'
+                }
+            },
+            sillyseals_primaryseal_seal = {
+                name = 'Primary Seal',
+                text = {
+		        	'When {C:attention}discarded{}, {C:attention}all cards{} in hand',
+		        	'gain {C:blue}+#1#{} and {X:blue,C:white}X#2#{} extra chips',
+                    '{C:inactive,s:0.75}(Also rounds extra chips to the nearest integer)'
+                }
+            },
+            sillyseals_platinumseal_seal = {
+                name = 'Platinum Seal',
+                text = {
+	            	'Earn {C:gold}$8{} for every {C:purple}tarot{} card in',
+	            	'your {C:attention}consumable tray{} when scored'
+                }
+            },
+            sillyseals_peachseal_seal = {
+                name = 'Peach Seal',
+                text = {
+                    '{C:attention}Create{} a {C:spectral}Soul{} when discarded',
+                    'if it is the {C:attention}last discard',
+                    'of the round',
+                    '{C:dark_edition,s:0.7,E:2}Idea by : SMG9000{}'
+                }
+            },
+            sillyseals_rainbowseal_seal = {
+                name = 'Rainbow Seal',
+                text = {
+                    'Gives {C:attention}#1# round(s){} of progress to',
+                    '{C:attention}all held {C:colourcard}Colour{} cards if held',
+                    'in hand'
+                }
+            },
+            sillyseals_wildseal_seal = {
+                name = 'Wild Seal',
+                text = {
+                    'When {C:attention}discarded{}, {C:attention}all owned{} playing cards',
+                    'gain {C:blue}+#1#{} and {X:blue,C:white}X#2#{} extra chips',
+                    '{C:attention}Wild Cards{} gain {C:blue}+#3#{} and {X:blue,C:white}X#4#{} extra chips',
+                    '{C:inactive,s:0.75}(Also rounds extra chips to the nearest integer)'
+                }
+            },
+            sillyseals_midnightseal_seal = {
+                name = 'Midnight Seal',
+                text = {
+                    '{C:attention}Multiplies the {C:planet}levels{} of all',
+                    'hands by {C:attention}#1#{} if {C:attention}held in hand{} at',
+                    'end of round'
+                }
+            },
+            sillyseals_bloodseal_seal = {
+                name = 'Blood Seal',
+                text = {
+                    '{C:red}Destroy{} the {C:attention}lowest rarity Joker{} in your',
+                    'possession and {C:attention}exponentiate {C:blue}Chips{} and {C:red}Mult{} of',
+                    'the played hand by {X:dark_edition,C:white}^2x{} when played and scoring',
+                    '{C:inactive,s:0.75}(x = rarity tier)',
+                    '{C:inactive,s:0.75}(If there are multiple jokers of this rarity',
+                    '{C:inactive,s:0.75}in your possession, the leftmost joker will be',
+                    '{C:inactive,s:0.75}destroyed)'
+                }
+            },
+            sillyseals_violetseal_seal = {
+                name = 'Violet Seal',
+                text = {
+                    'Create {C:attention}#1# {C:edition}hidden {C:spectral}Spectral(s){}',
+                    'plus {C:attention}1{} for {C:attention}each tag{} you have, then',
+                    '{C:red}destroy{} this card if played and scoring'
+                }
+            },
+            sillyseals_diamondseal_seal = {
+                name = 'Diamond Seal',
+                text = {
+                    'Create a {C:edition}Negative {C:cry_epic}Negative',
+                    '{C:cry_epic}Joker{} and earn {C:gold}$25{} when played',
+                    'and scoring'
+                }
+            },
+            sillyseals_tealseal_seal = {
+                name = 'Teal Seal',
+                text = {
+                    'If the {C:attention}ante{} is a {C:attention}multiple',
+                    'of {C:attention}4{} and the {C:attention}played hand{} has',
+                    'at least {C:attention}5 card(s){}, create a random {C:cry_exotic}Exotic',
+                    '{C:attention}joker{}, apply a {C:cry_exotic}Teal Seal{} to a {C:attention}random',
+                    'card in your deck and {C:red}destroy{} all played',
+                    'cards after scoring',
+                    '{C:inactive,s:0.75}(Can only activate once per hand)'
+                }
+            }
+        },
+    },
+    misc = {
+        labels = {
+            sillyseals_hotpinkseal_seal = 'Hot Pink Seal',
+            sillyseals_deepblueseal_seal = 'Deep Blue Seal',
+            sillyseals_lilacseal_seal = 'Lilac Seal',
+            sillyseals_magentaseal_seal = 'Magenta Seal',
+            sillyseals_limeseal_seal = 'Lime Seal',
+            sillyseals_neapolitanseal_seal = 'Neapolitan Seal',
+            sillyseals_gemstoneseal_seal = 'Gemstone Seal',
+            sillyseals_infernalseal_seal = 'Infernal Seal',
+            sillyseals_solarseal_seal = 'Solar Seal',
+            sillyseals_switchseal_seal = 'Switch Seal',
+            sillyseals_primaryseal_seal = 'Primary Seal',
+            sillyseals_platinumseal_seal = 'Platinum Seal',
+            sillyseals_peachseal_seal = 'Peach Seal',
+            sillyseals_rainbowseal_seal = 'Rainbow Seal',
+            sillyseals_wildseal_seal = 'Wild Seal',
+            sillyseals_midnightseal_seal = 'Midnight Seal',
+            sillyseals_bloodseal_seal = 'Blood Seal',
+            sillyseals_violetseal_seal = 'Violet Seal',
+            sillyseals_diamondseal_seal = 'Diamond Seal',
+            sillyseals_tealseal_seal = 'Teal Seal',
+        },
+        dictionary = {
+            k_sealspectralpack = "Seal Spectral Pack",
+        },
+    }
+}
